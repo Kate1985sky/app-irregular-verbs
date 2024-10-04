766a4b1003332dcb031f599b82b3f721afe8197c
katerina <korickaakaterina@gmail.com>
2024-10-04 15:42:10 +0100

    remove styles

--- src/pages/WordsByLevel/WordsByLevel.js
+++ src/pages/WordsByLevel/WordsByLevel.js
@@ -27,7 +27,7 @@ export const WordsByLevel = () => {
         {filteredByLevel.length > 0 ? (
           <ul className="w-50 row row-cols-1 row-cols-md-2 p-2">
             {filteredByLevel.map((card) => (
-              <Col xs="12" sm="6">
+              <Col key={card.id} xs="12" sm="6">
                 <OneCard card={card} key={card.id} />
               </Col>
             ))}
